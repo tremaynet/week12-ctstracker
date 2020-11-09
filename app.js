ac8a5bfff25c8cde57d92ef7817abc6afdd90bca
Tremayne Terry <tremaynet@gmail.com>
2020-11-08 19:13:55 -0500

    completed constructing the promptMessage object

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ const promptMesssage = {
     viewAllEmployees: "View All Employees",
     viewByDepartment: "View All Emplyees By Department",
     viewByManager: "View All Employees By Manager",
-    
-}
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Emplyee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viaAllRoles: "View All Roles",
+    exit: "Exit",
+};
